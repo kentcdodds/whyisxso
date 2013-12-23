a43de7866d0edb49e05fee0302ed2cb40130121f
Kent C. Dodds <kent@doddsfamily.us>
2013-12-23 09:39:40 -0700

    Forget this. Just allow everyone...

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var io = require('socket.io').listen(app);
 // io.set('origins', 'http://kent.doddsfamily.us');
-io.set('origins', 'http://kent.doddsfamily.us');
+io.set('origins', '*:*');
 app.listen(port, ip);
 
 var request = require('request');
