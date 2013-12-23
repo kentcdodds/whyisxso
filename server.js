5696f465db03bccb733d4ba24a1d6006b03d11cf
Kent C. Dodds <kent@doddsfamily.us>
2013-12-23 09:05:28 -0700

    Changing origin to include port...

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var io = require('socket.io').listen(app);
-io.set("origins","*");
+io.set('origins', '*:*');
 app.listen(port, ip);
 
 var request = require('request');
